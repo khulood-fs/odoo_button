9b69d397f3d3f5c5e2c56163aa16619f9bd05dc1
keid-fs <156651157+khulood-fs@users.noreply.github.com>
2024-07-22 13:55:28 +0400

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,12 +1,4 @@
-from odoo import api, fields, models
+from odoo import models, api
 
-class MyModel(models.Model):
-    _name = 'my_module.my_model'
-    _description = 'My Module Model'
-
-    name = fields.Char(string='Name')
-
-    @api.model
-    def test_code_v17(self):
-        # Your custom logic here
-        return True
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
