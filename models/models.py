33f3e65b45b286bb477401e2fefe77f85c0f605c
keid-fs <156651157+khulood-fs@users.noreply.github.com>
2024-07-22 14:57:01 +0400

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,4 +1,14 @@
 from odoo import models, api
+from odoo.exceptions import UserError
 
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    @api.model
+    def test_code_v17(self):
+        # Print to server logs
+        _logger = models.logging.getLogger(__name__)
+        _logger.info("OK")
+
+        # Raise a UserError to display a message in the UI
+        raise UserError("OK")
