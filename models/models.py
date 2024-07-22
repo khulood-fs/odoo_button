1f65536c0c0ef89ae4337772c2bd4286b882fa0b
keid-fs <156651157+khulood-fs@users.noreply.github.com>
2024-07-22 15:17:54 +0400

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,13 +1,13 @@
 from odoo import models, api
 from odoo.exceptions import UserError
+import logging
 
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    @api.model
     def test_code_v17(self):
         # Print to server logs
-        _logger = models.logging.getLogger(__name__)
+        _logger = logging.getLogger(__name__)
         _logger.info("OK")
 
         # Raise a UserError to display a message in the UI
