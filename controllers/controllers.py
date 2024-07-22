61270a16f84973d6c04960e1f442473ac62d2e2b
keid-fs <156651157+khulood-fs@users.noreply.github.com>
2024-07-22 13:50:51 +0400

    Update controllers.py

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,22 +1,14 @@
-# -*- coding: utf-8 -*-
-# from odoo import http
-
-
-# class MyModule(http.Controller):
-#     @http.route('/my_module/my_module', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/my_module/my_module/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('my_module.listing', {
-#             'root': '/my_module/my_module',
-#             'objects': http.request.env['my_module.my_module'].search([]),
-#         })
-
-#     @http.route('/my_module/my_module/objects/<model("my_module.my_module"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('my_module.object', {
-#             'object': obj
-#         })
-
+{
+    'name': 'My Module',
+    'version': '1.0',
+    'author': 'Your Name',
+    'category': 'Uncategorized',
+    'depends': ['base'],
+    'data': [
+        'views/my_model_views.xml',
+    ],
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    
